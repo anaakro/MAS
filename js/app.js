da8e5acb7fb098195ab17b1b21ead35934dee6eb
anaakro <117375234+anaakro@users.noreply.github.com>
2022-11-24 17:27:15 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -241,9 +241,7 @@ function readFiles(files) {
 
     item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
     target.appendChild(item);
- 
-let cache = window.caches.open(key);
-cache.put(request, response);
+ Cache.add(request);
   
   };
 }
