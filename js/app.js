03608f655f8ad22f2bf926e0fdf711366cc15ddc
anaakro <117375234+anaakro@users.noreply.github.com>
2023-01-19 16:07:59 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -411,3 +411,19 @@ if ('permissions' in navigator) {
         navigator.getUserMedia({audio: true}, noop, noop)
     }
 }
+
+    document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline';
+
+    var target = document.getElementById('target');
+
+    function handleStateChange() {
+      var timeBadge = new Date().toTimeString().split(' ')[0];
+      var newState = document.createElement('p');
+      var state = navigator.onLine ? 'online' : 'offline';
+      newState.innerHTML = '' + timeBadge + ' Connection state changed to ' + state + '.';
+      target.appendChild(newState);
+    }
+
+    window.addEventListener('online', handleStateChange);
+    window.addEventListener('offline', handleStateChange);
+
